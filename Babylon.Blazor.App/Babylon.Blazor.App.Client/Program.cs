b9a274c01986ffcc513d5b13b87cfe5e2fa47837
AlexNek <alexnek@russinger.com>
2023-11-27 20:00:37 +0100

    library workaround

--- Babylon.Blazor.App/Babylon.Blazor.App.Client/Program.cs
+++ Babylon.Blazor.App/Babylon.Blazor.App.Client/Program.cs
@@ -1,8 +1,14 @@
 using Babylon.Blazor;
+
+using BabylonBlazor.AppShared.Pages;
+
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+//JUST DO A STATIC REFERENCE HERE ! Workaround about Razor library connection
+var type = typeof(Water);
 
 builder.Services.AddTransient<InstanceCreatorBase>(sp => new InstanceCreator(sp.GetService<IJSRuntime>()));
 
